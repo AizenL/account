5a2d036f65c6880e062770e312b3ef03c5d07082
valentinthirion <valentinthirion@gmail.com>
2016-10-18 09:49:23 +0200

    [ADD] report_agedbalancedetail Added module to repo

---
+++ report_agedbalancedetail/__openerp__.py
@@ -0,0 +1,18 @@
+{
+   'name': "Aged partner balance detail report",
+    'version': '9.0.1.0.0',
+    'depends': ['account', 'account_reports'],
+    'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Accounting',
+    'description': 
+    """
+Aged partner balance detail report.
+
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
+    """,
+    'data': [
+        'reports/report_agedbalancedetail.xml',
+        'wizards/account_aged_balance_detail_view.xml',
+        ],
+}
