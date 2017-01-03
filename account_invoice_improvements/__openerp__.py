cc95c370be1f22567bfe3b7d68002fbbba7867fc
Valentin Thirion <vt@abakusitsolutions.eu>
2017-01-03 11:49:09 +0100

    [ADD] account_invoice_improvement Added module to repo and fixed the problem of non marked as paid invoices

---
+++ account_invoice_improvements/__openerp__.py
@@ -0,0 +1,30 @@
+{
+    'name': "AbAKUS invoice improvements",
+    'version': '9.0.1.0.3',
+    'depends': ['account'],
+    'author': "Valentin THIRION & Bernard DELHEZ, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Accounting',
+    'description': 
+    """
+    Invoicing improvements
+
+    This modules adds some functionalities to the invoicing process for AbAKUS. 
+        - it adds a field in the invoice form with the next invoice number.
+        - it auto copies the supplier ref number to the bank transfer communication field.
+        - it checks if the supplier invoice number already exists.
+        - it adds a group by in account.move.line that groups by journal entry.
+        - it changes the fields order in the treeview of account.move.line.
+        - it adds a filter that filters on customer accont related move lines on lines
+        - it adds a filter that filters on supplier accont related move lines on lines
+        - it checks if all the lines of the invoices contains at least one tax
+        - it checks if the 'analytic account' field is set
+
+    This module has been developed by Valentin THIRION @ AbAKUS it-solution.
+    """,
+    'data': [
+        'views/account_invoice_view.xml',
+        'views/account_move_line_view.xml',
+        'data/invoice_workflow.xml'
+    ],
+}
---
+++ account_invoice_improvements/models/account_invoice.py
@@ -0,0 +1,74 @@
+from openerp import models, fields, api, _
+from openerp.exceptions import ValidationError
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class account_invoice_improved(models.Model):
+    _inherit = ['account.invoice']
+
+    next_invoice_number = fields.Char(compute='_compute_next_invoice_number', string="Next document number", store=False)
+
+    @api.one
+    @api.depends('journal_id')
+    def _compute_next_invoice_number(self):
+        if not self.journal_id:
+            self.next_invoice_number =  "NO JOURNAL SELECTED"
+            return
+
+        if self.state in ("", "draft"):
+            nn_int = self.journal_id.sequence_id.number_next
+            nn_string = str(nn_int)
+            
+            nn_ex_string = self.env['account.invoice'].search([['journal_id','=',self.journal_id.id],['number', '!=', '']], limit=1).number
+
+            l = len(nn_ex_string) - 1
+            if l > 0:
+                cpt = 0;
+                for i in range(l, 0, -1):
+                    if nn_ex_string[i].isnumeric():
+                        cpt += 1
+                    else:
+                        break
+                seq = int(nn_ex_string[-cpt:]) + 1
+                nn = nn_ex_string[:-cpt]
+                self.next_invoice_number =  nn + str(seq)
+            else:
+                self.next_invoice_number =  '1'
+        else:
+            self.next_invoice_number =  "ERROR"
+    
+    @api.onchange('supplier_invoice_number')
+    def update_reference(self):
+        if self.partner_id and self.supplier_invoice_number and len(self.supplier_invoice_number) > 0:
+            account_invoices = self.env['account.invoice'].search([[('supplier_invoice_number', '=', self.supplier_invoice_number),('partner_id','=',self.partner_id.id)]])
+            if account_invoices:
+                self.reference = ""
+                raise ValidationError('Supplier Invoice Number Failure - The supplier invoice number already exists')
+            else:
+                self.reference = self.supplier_invoice_number
+        else:
+            self.reference = self.supplier_invoice_number
+
+    @api.multi
+    def check_validate_and_send_invoice_if_out(self):
+        for line in self.invoice_line_ids:
+            # Check if there is a tax on each line
+            if len(line.invoice_line_tax_ids) == 0:
+                raise ValidationError('No tax! - A line in this invoice does not contain any tax. This is not allowed by the system. Please, correct this.')
+                self.write({'state': 'draft'})
+            # Check if there is an analytic account on each line
+            if len(line.account_analytic_id) == 0:
+                raise ValidationError('No Analytic Account! - A line in this invoice does not contain an analytic account. This is not allowed by the system. Please, correct this.')
+                self.write({'state': 'draft'})
+
+        #Methods for the validation of the invoice.
+        self.action_date_assign()
+        self.action_move_create()
+
+        if self.type == 'out_invoice':
+            self.invoice_validate()
+            #Method that return the mail form.
+            return self.action_invoice_sent()
+        return self.invoice_validate()
