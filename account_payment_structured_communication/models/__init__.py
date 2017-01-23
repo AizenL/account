5e2bd957f8de781d4d18d88a1d66eb398c454c55
Valentin Thirion <vt@abakusitsolutions.eu>
2017-01-23 16:32:34 +0100

    [ADD] account_payment_structured_communication Added field on invoice for structured communication

--- account_payment_structured_communication/models/__init__.py
+++ account_payment_structured_communication/models/__init__.py
@@ -1,1 +1,2 @@
 from . import payment_order
+from . import account_invoice
---
+++ account_payment_structured_communication/models/account_invoice.py
@@ -0,0 +1,10 @@
+from openerp import models, fields, api, _
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    supplier_reference = fields.Char(string="Supplier reference")        
+    
