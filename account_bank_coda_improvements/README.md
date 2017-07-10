606d640f619a6ba70f1c3b20369f26689b3e24dd
Valentin Thirion <vt@abakusitsolutions.eu>
2017-07-10 12:37:22 +0200

    [ADD] account_bank_coda_improvements Added module to repo

---
+++ account_bank_coda_improvements/README.md
@@ -0,0 +1,6 @@
+#CODA Import : Bank statement improvements
+
+This modules adds:
+- a field in the bank statement form that show the balance of the corresponding account for the bank.
+
+This module has been developed by Valentin Thirion @ AbAKUS it-solutions
---
+++ account_bank_coda_improvements/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': "AbAKUS Bank Statement improvements",
+    'version': '9.0.1.0',
+    'depends': [
+        'account',
+        'l10n_be_coda'
+    ],
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Accounting',
+    'description': 
+    """
+    This modules adds a field in the bank statement form that show the balance of the corresponding account for the bank.
+
+    """,
+    'data': ['view/account.bank.statement.form.xml'],
+}
