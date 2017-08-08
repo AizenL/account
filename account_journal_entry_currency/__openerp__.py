a66563230f4abb6f5c41d3a8d5ab5f1462f77a3e
jasonpindat <j.pindat@hotmail.fr>
2017-08-08 11:18:20 +0200

    [ADD] account_journal_entry_currency Added new module to the repo

---
+++ account_journal_entry_currency/__openerp__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Account Journal Entry Currency",
+    'version': '9.0.1.0.0',
+    'depends': [
+        'account'
+    ],
+    'author': "Jason PINDAT @ AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Accounting',
+    'description': """
+        Account Journal Entry Currency
+
+        Sets currency to company's currency by default
+
+        This module has been developed by Jason PINDAT @ AbAKUS it-solutions.
+    """,
+    'data': [
+        'views/account_move.xml'
+    ],
+}
