8c29f33f7b0c0100f049fb813b70ef31fd20e1f2
jasonpindat <j.pindat@hotmail.fr>
2017-08-22 17:20:05 +0200

    test

--- account_invoice_line_account/models/invoice.py
+++ account_invoice_line_account/models/invoice.py
@@ -6,6 +6,20 @@ _logger = logging.getLogger(__name__)
 class AccountInvoiceLine(models.Model):
     _inherit = ['account.invoice.line']
 
+    def _compute_values(self, values):
+
+        product = self.env['product.product'].search([('id', '=', values['product_id'])]) if values.get('product_id') else self.product_id
+
+        _logger.info('\n\n' + str(product) + '\n\n')
+
+        return values
+
     @api.model
     def create(self, values):
+        values = self._compute_values(values)
         return super(AccountInvoiceLine, self).create(values)
+
+    @api.one
+    def write(self, values):
+        values = self._compute_values(values)
+        return super(AccountInvoiceLine, self).write(values)
