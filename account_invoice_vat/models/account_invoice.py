a1424b368dc0a86e3157f12fc436ed6b5f2fdc85
Valentin Thirion <vt@abakusitsolutions.eu>
2017-08-22 10:42:23 +0200

    [FIX] account_invoice_vat Set the field as auto computed but independent (so we can filter on it) Set the field readonly

--- account_invoice_vat/models/account_invoice.py
+++ account_invoice_vat/models/account_invoice.py
@@ -8,9 +8,10 @@ _logger = logging.getLogger(__name__)
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    partner_vat = fields.Char(string="Vendor's TIN", compute='_compute_partner_vat')
+    partner_vat = fields.Char(string="Vendor's TIN")
 
-    @api.one
-    @api.depends('partner_id')
-    def _compute_partner_vat(self):
-        self.partner_vat = self.partner_id.vat
+    @api.multi
+    @api.onchange('partner_id')
+    def compute_partner_vat(self):
+        for invoice in self:
+            invoice.partner_vat = invoice.partner_id.vat
