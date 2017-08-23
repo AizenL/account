9ab4c6fd7a2a66dae6e0cebbc87d00fb8cc6d866
jasonpindat <j.pindat@hotmail.fr>
2017-08-23 10:23:37 +0200

    test

--- account_invoice_line_account/models/invoice.py
+++ account_invoice_line_account/models/invoice.py
@@ -10,17 +10,21 @@ class AccountInvoiceLine(models.Model):
     def create(self, values):
         product = self.env['product.product'].search([('id', '=', values['product_id'])]) if values.get('product_id') else self.product_id
 
+        config_error = False
+
         if product and product.type != 'service':
             if product.income_analytic_account_id:
                 values['account_analytic_id'] = product.income_analytic_account_id.id
             elif product.categ_id and product.categ_id and product.categ_id.income_analytic_account_id:
                 values['account_analytic_id'] = product.categ_id.income_analytic_account_id.id
             else:
-                values['account_analytic_id'] = False
+                config_error = True
 
-        values['account_analytic_id'] = False
+        config_error = True
 
-        if values.get('account_analytic_id'):
-            _logger.info('\n\n' + str(values['account_analytic_id']) + '\n\n')
+        record = super(AccountInvoiceLine, self).create(values)
 
-        return super(AccountInvoiceLine, self).create(values)
+        if config_error:
+            record.account_analytic_id = False
+
+        return record
