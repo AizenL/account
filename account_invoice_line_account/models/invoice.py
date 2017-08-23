f13601ab3457807918e74a1dd020d6b3a1d6cf03
jasonpindat <j.pindat@hotmail.fr>
2017-08-23 09:46:54 +0200

    test

--- account_invoice_line_account/models/invoice.py
+++ account_invoice_line_account/models/invoice.py
@@ -10,18 +10,27 @@ class AccountInvoiceLine(models.Model):
 
         product = self.env['product.product'].search([('id', '=', values['product_id'])]) if values.get('product_id') else self.product_id
 
+        # if product:
+        #     if product.type != 'service':
+        #         if 
+
         _logger.info('\n\n' + str(product) + '\n\n')
 
         _logger.info('\n\n' + str(product.type) + '\n\n')
+
+        if(values.get('income_analytic_account_id'))
+            _logger.info('\n\n' + str(self.env['account.analytic.account'].search([('id', '=', values['income_analytic_account_id'])]).name) + '\n\n')
 
         return values
 
     @api.model
     def create(self, values):
+        _logger.info('\n\nCREATE\n\n')
         values = self._compute_values(values)
         return super(AccountInvoiceLine, self).create(values)
 
     @api.one
     def write(self, values):
+        _logger.info('\n\nWRITE\n\n')
         values = self._compute_values(values)
         return super(AccountInvoiceLine, self).write(values)
